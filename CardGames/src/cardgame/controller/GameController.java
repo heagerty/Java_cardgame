5b80d7cfa030cefbbfa0c1b180babb7bbfd3a0a1
Casey Heagerty <cheagerty@gmail.com>
2020-04-14 14:32:23 +0200

    Refactor game evaluator

--- CardGames/src/cardgame/controller/GameController.java
+++ CardGames/src/cardgame/controller/GameController.java
@@ -6,6 +6,7 @@ import cardgame.model.Deck;
 import cardgame.model.Player;
 import cardgame.model.PlayingCard;
 import cardgame.view.View;
+import cardgames.gamedata.GameEvaluator;
 
 public class GameController {
 	
@@ -83,37 +84,7 @@ public class GameController {
 	}
 	
 	void evaluateWinner() {
-		Player bestPlayer = null;
-		int bestRank = -1;
-		int bestSuit = -1;
-		
-		for (Player player : players) {
-			boolean newBestPlayer = false;
-			
-			if (bestPlayer == null) {
-				newBestPlayer = true;
-			}
-			else {
-				PlayingCard pc = player.getCard(0);
-				int thisRank = pc.getRank().value();
-				if (thisRank >= bestRank) {
-					if (thisRank > bestRank) {
-						newBestPlayer = true;
-					} else {
-						if (pc.getSuit().value() > bestSuit) {
-							newBestPlayer = true;
-						}
-					}
-				}
-			}
-			if (newBestPlayer) {
-				bestPlayer = player;
-				PlayingCard pc = player.getCard(0);
-				bestRank = pc.getRank().value();
-				bestSuit = pc.getSuit().value();
-			}
-		}
-		winner = bestPlayer;
+		winner = new GameEvaluator().evaluateWinner(players);
 	}
 	
 	void displayWinner() {
